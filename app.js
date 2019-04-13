1a79f9e56f7ef6a42c3b4edc426f3147d2712e48
Hariix_ <>
2019-04-13 18:53:15 +0200

    MySQL > PostGreSQL

--- app.js
+++ app.js
@@ -7,12 +7,12 @@
 require("dotenv").config();
 
 const { CommandHandler } = require("djs-commands");
+const { Pool, Client } = require("pg");
 const hexToDec = require("./modules/hexConverter");
 const Discord = require("discord.js");
 const request = require("request");
 const sparkly = require("sparkly");
 const chalk = require("chalk");
-const mysql = require("mysql");
 
 const config = require("./assets/config.json");
 const converter = require("./modules/data");
@@ -29,6 +29,15 @@ if(parseInt(process.versions.node.split(".")[0]) < 8) {
 	});
 };
 
+const db = new Pool({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	port: process.env.DB_PORT,
+	keepAlive: true
+});
+
 const bot = new Discord.Client({
 	disableEveryone: true
 });
@@ -37,21 +46,6 @@ const CH = new CommandHandler({
 	folder: __dirname + "/commands/",
 	prefix: ["/"]
 });
-
-var db = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	port: process.env.DB_PORT,
-	timezone: "Europe/Paris",
-	charset: config.encode,
-	typeCast: true,
-	debug: false,
-	supportBigNumbers: true
-});
-
-var queued = {};
 
 const log = (message, level) => logger(message, level, bot, __filename);
 
@@ -69,9 +63,9 @@ function createServer(guild) {
 	db.query("SELECT server_id FROM servers_settings WHERE server_id = " + guild.id + ";", function(err, result, fields) {
 		if(err) log("Une erreur est survenue lors de la recherche du serveur: " + err, "FATAL");
 
-		if((!result) || (!result[0]) || (!result[0].server_id)) {
+		if((!result) || (!result.rows[0]) || (!result.rows[0].server_id)) {
 			//options must be converted to decimal: https://www.rapidtables.com/convert/number/binary-to-decimal.html
-			db.query(`INSERT INTO servers_settings VALUES (${guild.id}, '${config.default.prefix}', '${config.default.lang}', '${config.default.currency}', ${config.default.money}, 0, ${config.default.options});`, function(err, result, fields) {
+			db.query(`INSERT INTO servers_settings (server_id, prefix, lang, money_char, start_money, announcements_channel_id, options) VALUES (${guild.id}, '${config.default.prefix}', '${config.default.lang}', '${config.default.currency}', ${config.default.money}, 0, ${config.default.options});`, function(err, result, fields) {
 				if(err) return log("Une erreur est survenue lors de l'enregistrement du serveur: " + err, "ERROR");
 			});
 		};
@@ -88,7 +82,7 @@ db.init = function() {
 	 * options: 0 à 16777215,
 	 */
 
-	db.query("CREATE TABLE IF NOT EXISTS servers_settings (server_id BIGINT UNSIGNED NOT NULL, prefix VARCHAR(5) NOT NULL, lang CHAR(2) NOT NULL, money_char VARCHAR(3) NOT NULL, start_money INT UNSIGNED NOT NULL, announcements_channel_id BIGINT UNSIGNED NOT NULL, options MEDIUMINT UNSIGNED NOT NULL, PRIMARY KEY (server_id)) ENGINE = INNODB;", function(err, result, fields) {
+	db.query("CREATE TABLE IF NOT EXISTS servers_settings (server_id BIGINT NOT NULL, prefix VARCHAR(5) NOT NULL, lang CHAR(2) NOT NULL, money_char VARCHAR(3) NOT NULL, start_money INT NOT NULL, announcements_channel_id BIGINT NOT NULL, options BIGINT NOT NULL, PRIMARY KEY (server_id));", function(err, result, fields) {
 		if(err) return log("La table " + chalk.green("'servers_settings'") + " n'a pas pû être créé: " + err, "FATAL");
 		log("La table " + chalk.green("'servers_settings'") + " à été vérifiée.", "INFO");
 	});
@@ -104,19 +98,19 @@ db.init = function() {
 	 * kicks: 0 à 65535,
 	 */
 
-	db.query("CREATE TABLE IF NOT EXISTS users (id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT, user_id BIGINT UNSIGNED NOT NULL, server_id BIGINT UNSIGNED NOT NULL, lang VARCHAR(2) NOT NULL, messages BIGINT UNSIGNED NOT NULL, caracters BIGINT UNSIGNED NOT NULL, money BIGINT UNSIGNED NOT NULL, xp INT UNSIGNED NOT NULL, level SMALLINT UNSIGNED NOT NULL, bans SMALLINT UNSIGNED NOT NULL, kicks SMALLINT UNSIGNED NOT NULL, PRIMARY KEY (id)) ENGINE = INNODB;", function(err, result, fields) {
+	db.query("CREATE TABLE IF NOT EXISTS users (id SERIAL NOT NULL, user_id BIGINT NOT NULL, server_id BIGINT NOT NULL, lang VARCHAR(2) NOT NULL, messages BIGINT NOT NULL, caracters BIGINT NOT NULL, money BIGINT NOT NULL, xp INT NOT NULL, level SMALLINT NOT NULL, bans SMALLINT NOT NULL, kicks SMALLINT NOT NULL, PRIMARY KEY (id));", function(err, result, fields) {
 		if(err) return log("La table " + chalk.green("'users'") + " n'a pas pû être créé: " + err, "FATAL");
 		log("La table " + chalk.green("'users'") + " à été vérifiée.", "INFO");
 	});
 
 	/**
-	 * banned_at: DATETIME,
-	 * revoke_at: DATETIME,
+	 * banned_at: DATE,
+	 * revoke_at: DATE,
 	 * admin_id: 0 à 18446744073709551615,
 	 * reason: 255 caractères max,
 	 */
 
-	db.query("CREATE TABLE IF NOT EXISTS bans (id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT, user_id BIGINT UNSIGNED NOT NULL, server_id BIGINT UNSIGNED NOT NULL, banned_at DATETIME NOT NULL, revoke_at DATETIME NOT NULL, admin_id BIGINT UNSIGNED NOT NULL, reason VARCHAR(255), PRIMARY KEY (id)) ENGINE = INNODB;", function(err, result, fiels) {
+	db.query("CREATE TABLE IF NOT EXISTS bans (id SERIAL NOT NULL, user_id BIGINT NOT NULL, server_id BIGINT NOT NULL, banned_at DATE NOT NULL, revoke_at DATE NOT NULL, admin_id BIGINT NOT NULL, reason VARCHAR(255), PRIMARY KEY (id));", function(err, result, fiels) {
 		if(err) return log("La table " + chalk.green("'bans'") + " n'a pas pû être créé: " + err, "FATAL");
 		log("La table " + chalk.green("'bans'") + " à été vérifiée.", "INFO");
 	});
@@ -144,23 +138,22 @@ bot.on("message", message => {
 		};
 	};
 
-	log("<" + message.guild.roles);
 	db.query("SELECT prefix, lang, money_char, start_money, announcements_channel_id, options FROM servers_settings WHERE server_id = '" + message.guild.id + "';", function(err, result, fields) {
 		if(err) return log("Les paramètres du serveur " + chalk.green("'" + message.guild.id + "'") + " n'ont pas pû être obtenus: " + err, "FATAL");
 
-		if((!result) || (!result[0]) || (!result[0].prefix) || (!result[0].lang)) return message.channel.send(new Discord.RichEmbed({
+		if((!result) || (!result.rows[0]) || (!result.rows[0].prefix) || (!result.rows[0].lang)) return message.channel.send(new Discord.RichEmbed({
 			title: ":x: Veuillez patienter, s'il vous plaît.",
 			description: "La base de données sera prête au prochain message. Une mise à jour est responsable de cette erreur. Il se peux que les données utilisateurs soient aussi supprimée, si tel est le cas le prochain message créera votre compte. Excusez nous pour ce dérangement, cette erreur est rare. Le responsable de ce serveur peux faire une demande de récupération de données, mais rien n'est garanti... Support prioritaire: vincent.bernhardt67@gmail.com",
 			color: hexToDec.hexToDec(colors.red)
 		}));
 
 		var settings = {
-			defaultLang: result[0].lang,
-			prefix: result[0].prefix,
-			currency: result[0].money_char,
-			startMoney: result[0].start_money,
-			announcementsChannel: result[0].announcements_channel_id,
-			plugins: result[0].options
+			defaultLang: result.rows[0].lang,
+			prefix: result.rows[0].prefix,
+			currency: result.rows[0].money_char,
+			startMoney: result.rows[0].start_money,
+			announcementsChannel: result.rows[0].announcements_channel_id,
+			plugins: result.rows[0].options
 		};
 
 		CH.prefix = settings.prefix;
@@ -168,8 +161,8 @@ bot.on("message", message => {
 		db.query("SELECT id, lang, messages, caracters, money, xp, level, bans, kicks FROM users WHERE user_id = " + message.author.id + " AND server_id = " + message.guild.id + ";", function(err, result, fields) {
 			if(err) return log("message: Une erreur est survenue lors de la recherche de l'utilisateur: " + err, "FATAL");
 	
-			if((!result) || (!result[0]) || (!result[0].id) || (!result[0].lang)) {
-				db.query("INSERT INTO users VALUES (null, " + message.author.id + ", " + message.guild.id + ", '" + settings.defaultLang + "', 0, 0, " + settings.startMoney + ", 0, 0, 0, 0)", function(err, result, fields) {
+			if((!result) || (!result.rows[0]) || (!result.rows[0].id) || (!result.rows[0].lang)) {
+				db.query("INSERT INTO users (user_id, server_id, lang, messages, caracters, money, xp, level, bans, kicks) VALUES (" + message.author.id + ", " + message.guild.id + ", '" + settings.defaultLang + "', 0, 0, " + settings.startMoney + ", 0, 0, 0, 0)", function(err, result, fields) {
 					if(err) return log("message: Une erreur est survenue lors de la création du compte de " + chalk.green(message.author.id) + " sur le serveur " + chalk.green(message.guild.id) + ": " + err, "ERROR");
 				});
 				return;
@@ -180,36 +173,27 @@ bot.on("message", message => {
 			var cmd = CH.getCommand(command);
 
 			var data = {
-				id: result[0].id,
-				lang: result[0].lang,
+				id: result.rows[0].id,
+				lang: result.rows[0].lang,
 				stats: {
-					messageSents: result[0].messages,
-					caractersSents: result[0].caracters,
-					money: result[0].money,
-					xp: result[0].xp,
-					level: result[0].level
+					messageSents: result.rows[0].messages,
+					caractersSents: result.rows[0].caracters,
+					money: result.rows[0].money,
+					xp: result.rows[0].xp,
+					level: result.rows[0].level
 				},
 				sanctions: {
-					bans: result[0].bans,
-					kicks: result[0].kicks
+					bans: result.rows[0].bans,
+					kicks: result.rows[0].kicks
 				}
 			};
 
 			if(!cmd) {
-				if(!queued[message.author.id]) {
-					queued[message.author.id] = {
-						sent: 0,
-						caracs: 0,
-						reward: 0
-					};
-				};
-				
-				queued[message.author.id].sent++;
-				queued[message.author.id].caracs += message.content.length;
+				let reward;
 
 				if(message.content.length >= 200) {
-					queued[message.author.id].reward += Math.floor(Math.random() * 20) + 1;
-				} else queued[message.author.id].reward += Math.floor(Math.random() * 15) + 1;
+					reward = Math.floor(Math.random() * 22) + 1;
+				} else reward = Math.floor(Math.random() * 17) + 1;
 
 				var serial = require("./assets/serial.json");
 				var pos = data.stats.level + 1;
@@ -234,21 +218,15 @@ bot.on("message", message => {
 					});
 				};
 
-				//XP and Levels updates every messages sents by users.
-				db.query("UPDATE users SET xp = xp + " + queued[message.author.id].reward + " WHERE id = " + data.id + ";", function(err, result, fields) {
-					queued[message.author.id].reward = 0;
+				//XP and Levels and now messages updates every messages sents by users.
+				db.query("UPDATE users SET xp = xp + " + reward + " WHERE id = " + data.id + ";", function(err, result, fields) {
+					reward = 0;
 					if(err) return log("La mise à jour du système d'expérience à rencontrée un problème: " + err, "ERROR");
 				});
 
-				if(queued[message.author.id].sent >= 5) {
-					//Statistics updates every 5 messages sents by users.
-					db.query("UPDATE users SET messages = messages + " + queued[message.author.id].sent + ", caracters = caracters + " + queued[message.author.id].caracs + " WHERE id = " + data.id + ";", function(err, result, fields) {
-						if(err) return log("La mise à jour des statistiques à rencontrée un problème: " + err, "ERROR");
-					});
-
-					queued[message.author.id].sent = 0;
-					queued[message.author.id].caracs = 0;
-				};
+				db.query("UPDATE users SET messages = messages + 1, caracters = caracters + " + message.content.length + " WHERE id = " + data.id + ";", function(err, result, fields) {
+					if(err) return log("La mise à jour des statistiques à rencontrée un problème: " + err, "ERROR");
+				});
 
 				return;
 			} else {
@@ -280,14 +258,14 @@ bot.on("guildMemberAdd", member => {
 	db.query("SELECT id FROM users WHERE user_id = '" + member.id + "' AND server_id = '" + member.guild.id + "';", function(err, result, fields) {
 		if(err) return log("guildMemberAdd: Une erreur est survenue lors de la recherche de l'utilisateur: " + err, "ERROR");
 
-		if((!result) || (!result[0]) || (!result[0].id)) {
+		if((!result) || (!result.rows[0]) || (!result.rows[0].id)) {
 			db.query("SELECT lang, start_money FROM servers_settings WHERE server_id = '" + member.guild.id + "';", function(err, result, fields) {
 				if(err) return log("guildMemberAdd: La langue par défault du serveur n'a pas pû être récupérée: " + err, "ERROR");
 
-				var defaultLang = result[0].lang;
-				var startMoney = result[0].start_money;
+				var defaultLang = result.rows[0].lang;
+				var startMoney = result.rows[0].start_money;
 
-				db.query("INSERT INTO users VALUES (null, " + member.id + ", " + member.guild.id + ", '" + defaultLang + "', 0, 0, " + startMoney + ", 0, 0, 0, 0)", function(err, result, fields) {
+				db.query("INSERT INTO users (user_id, server_id, lang, messages, caracters, money, xp, level, bans, kicks) VALUES (" + member.id + ", " + member.guild.id + ", '" + defaultLang + "', 0, 0, " + startMoney + ", 0, 0, 0, 0)", function(err, result, fields) {
 					if(err) return log("guildMemberAdd: Une erreur est survenue lors de la création du compte de " + chalk.green(member.id) + " sur le serveur " + chalk.green(member.guild.id) + ": " + err, "ERROR");
 				});
 			});
