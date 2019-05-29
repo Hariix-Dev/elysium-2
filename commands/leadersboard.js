a5f26faa332b48215ff1b2c379fc44b1fa5936ef
Hariix_ <>
2019-05-29 21:43:50 +0200

    up

---
+++ commands/leadersboard.js
@@ -0,0 +1,15 @@
+/* jshint esversion: 6 */
+/* jshint -W032 */
+/* jshint -W030*/
+
+module.exports = class leadersboard {
+	constructor() {
+		this.name = "leadersboard",
+		this.alias = [],
+		this.usage = "/leadersboard";
+	};
+
+	run(bot, message, args, data, settings, db) {
+		
+	};
+};
